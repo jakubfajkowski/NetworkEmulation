c5706f0eead5eeb6e1fb067962346102e542a3b2
Michal Kolodziej <M.Kolodziej@stud.elka.edu.pl>
2016-12-05 21:57:01 +0100

    [ClientNode] GUI, metody tworząca komórki ATM oraz odczytująca dane z komórek

---
+++ ClientNode/ClientNode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkUtilities;
+
+
+namespace ClientNode
+{
+    class ClientNode
+    {
+        public string message;
+        public string receivedMessage;
+        //lista przechowująca komórki ATM
+        public List<ATMCell> atmCells = new List<ATMCell>();
+
+        //metoda zamieniająca tekst na bity i dzieląca je na komórki ATM
+        public void createATMCell(int vpi, int vci, string message)
+        {
+            byte[] source = Encoding.UTF8.GetBytes(message);
+
+
+            for (int i = 0; i < source.Length; i += 48)
+            {
+                byte[] buffer = new byte[48];
+                if (i <= source.Length - 48)
+                {
+                    Buffer.BlockCopy(source, i, buffer, 0, 48);
+                    atmCells.Add(new ATMCell(vpi, vci, buffer));
+                }
+                else // gdy długość wiadomości jest mniejsza od 48 bitów, komórka jest wypełniana '0' na pozostałych miejscach
+                {
+                    Buffer.BlockCopy(source, i, buffer, 0, source.Length - i);
+                    atmCells.Add(new ATMCell(vpi, vci, buffer));
+                }
+            }
+        }
+
+        //odczytywanie i interpretowanie informacji charakterystycznej z komórek ATM
+        public void readDataFromATMCells()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < atmCells.Count; i++)
+            {
+                sb.Append(Encoding.UTF8.GetString(atmCells[i].data));
+            }
+            receivedMessage = sb.ToString();
+        }
+        
+
+    }
+}
--- ClientNode/Form1.Designer.cs
+++ ClientNode/Form1.Designer.cs
@@ -23,12 +23,133 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxReceived = new System.Windows.Forms.TextBox();
+            this.textBoxEventLog = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxClients = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(177, 127);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 25);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(240, 70);
+            this.textBoxMessage.TabIndex = 1;
+            this.textBoxMessage.TextChanged += new System.EventHandler(this.textBoxMessage_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxReceived
+            // 
+            this.textBoxReceived.Location = new System.Drawing.Point(262, 25);
+            this.textBoxReceived.Multiline = true;
+            this.textBoxReceived.Name = "textBoxReceived";
+            this.textBoxReceived.Size = new System.Drawing.Size(240, 70);
+            this.textBoxReceived.TabIndex = 3;
+            this.textBoxReceived.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBoxEventLog
+            // 
+            this.textBoxEventLog.Location = new System.Drawing.Point(12, 181);
+            this.textBoxEventLog.Multiline = true;
+            this.textBoxEventLog.Name = "textBoxEventLog";
+            this.textBoxEventLog.Size = new System.Drawing.Size(490, 70);
+            this.textBoxEventLog.TabIndex = 4;
+            this.textBoxEventLog.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Received message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Event Log";
+            // 
+            // comboBoxClients
+            // 
+            this.comboBoxClients.FormattingEnabled = true;
+            this.comboBoxClients.Location = new System.Drawing.Point(12, 127);
+            this.comboBoxClients.Name = "comboBoxClients";
+            this.comboBoxClients.Size = new System.Drawing.Size(136, 21);
+            this.comboBoxClients.TabIndex = 7;
+            this.comboBoxClients.SelectedIndexChanged += new System.EventHandler(this.comboBoxClients_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Avaliable clients";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(514, 263);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxClients);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxEventLog);
+            this.Controls.Add(this.textBoxReceived);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Form1";
+            this.Text = "ClientNode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxReceived;
+        private System.Windows.Forms.TextBox textBoxEventLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxClients;
+        private System.Windows.Forms.Label label4;
     }
 }
 
