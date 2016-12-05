c72a30855c3f2b3efbc98010de04d68a820aee32
Jarosław Steckiewicz <xitronix@mail.ru>
2016-12-05 14:22:17 +0100

    [NetworUtilities] Add generate Sockets methods (need use after load data from XML)

--- NetworkUtilities/Node.cs
+++ NetworkUtilities/Node.cs
@@ -36,11 +36,33 @@ namespace NetworkUtilities
                 string.Format("Can't connect to port {0} or port {1]!", cloudPort,agentPort));
             }
         }
+        //po wczytaniu z XML metody update generują sokety na podstawie poertów
+        public void updateCloudSocket() {
+            cloudSocket = updateSocket(cloudSocket, cloudPort);
+        }
+        public void updateNetworkManagerSocket()
+        {
+            networkManagerSocket = updateSocket(networkManagerSocket, agentPort);
+        }
+        private Socket updateSocket(Socket socket, int port) {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(generateIPEndPoint(port));
+                return socket;
+            }
+            catch (Exception e)
+            {
+                Debug.Fail(e.ToString(),
+                string.Format("Can't connect to port {0} or port {1]!", cloudPort, agentPort));
+                return null;
+            }
+        }
         public void connectToCloud(int port)
         {
 
         } 
-        public IPEndPoint generateIPEndPoint(int port)
+        private IPEndPoint generateIPEndPoint(int port)
         {
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList[0];
