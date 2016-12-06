e86300c391bba8ae1455a455636f987821fbf354
Jarosław Steckiewicz <xitronix@mail.ru>
2016-12-06 14:49:44 +0100

    [NetworkUtilities] Add test(NodeRecieve), change sockets to TCPLIstener and methods linked whith them

--- NetworkUtilities/Node.cs
+++ NetworkUtilities/Node.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
@@ -17,68 +16,138 @@ namespace NetworkUtilities
     {
         public int cloudPort;
         public int agentPort;
-        private Socket cloudSocket;
-        private Socket networkManagerSocket;
-
+        private byte[] bytes = new byte[256];
+        private TcpListener cloudTcpListener;
+        private TcpListener agentTcpListener;
+        /**
+         * Konstruktor niewykorzysywany
+         */
         private Node() { }
-
+        /**
+         * Konstruktor klasy Node wczytujący porty agenta i chmury i tworzący na ich podstawie @cloudSocket i @networkManagerSocket, po czym tworzone jest połączenie z chmurą
+         */
         public Node(int portA, int portC) {
             cloudPort = portC;
             agentPort = portA;
-            networkManagerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            cloudSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try {
-                networkManagerSocket.Bind(generateIPEndPoint(agentPort));
-                cloudSocket.Bind(generateIPEndPoint(cloudPort));
-                connectToCloud();
+                cloudTcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), cloudPort);
+                agentTcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), agentPort);
             } catch(Exception e){
                 Debug.Fail(e.ToString(),
                 string.Format("Can't connect to port {0} or port {1}!", cloudPort,agentPort));
             }
+            startListening(cloudTcpListener);
+            startListening(agentTcpListener);
+            connectToCloud();
         }
-        //po wczytaniu z XML metody update generują sokety na podstawie poertów
-        public void updateCloudSocket() {
-            cloudSocket = updateSocket(cloudSocket, cloudPort);
+        /**
+         * Metoda generująca @cloudSocket na podstawie portu @cloudPort 
+         */
+        public void updateCloudListener() {
+            cloudTcpListener = updateListener(cloudTcpListener, cloudPort);
         }
-        public void updateNetworkManagerSocket()
+        /**
+        * Metoda generująca @agentTcpListener na podstawie portu @agentPort
+        */
+        public void updateAgentTCPListener()
         {
-            networkManagerSocket = updateSocket(networkManagerSocket, agentPort);
+            agentTcpListener = updateListener(agentTcpListener, agentPort);
         }
-        private Socket updateSocket(Socket socket, int port) {
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        /**
+         * Metoda na danym etapie nasłuchuje na wchodzącą wiadomość i w razie przyjścia obsługuje go.(wysyła odpowiedź)
+         */
+        private void startListening(TcpListener tcpListener)
+        {
+            string data = null;
+            Task.Run(() =>
+            {
+                tcpListener.Start();
+                while (true)
+                {
+                    Debug.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Debug.WriteLine("Connected!");
+                    data = null;
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+                        Debug.WriteLine("Received: "+ data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Debug.WriteLine("Sent: {0}", data);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                    
+                }
+            });
+        }
+        /**
+         * Metoda która przypisuje port do socketa
+         * @param socket socket, do którego przypisujemy port
+         * @param port numer portu, na którym będzie działać socket
+         */ 
+        private TcpListener updateListener(TcpListener tcpListener, int port) {
             try
             {
-                socket.Bind(generateIPEndPoint(port));
-                return socket;
+                tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                return tcpListener;
             }
             catch (Exception e)
             {
                 Debug.Fail(e.ToString(),
-                string.Format("Can't connect to port {0} or port {1]!", cloudPort, agentPort));
+                    $"Can't connect to port {port}!");
                 return null;
             }
         }
+        /**
+         * Metoda wysyłająca cloudPort do chmury, na którym będzie nasłuchiwać
+         */
         public void connectToCloud() {
             UdpClient udpClient = new UdpClient();
             byte[] bytesToSend = Encoding.ASCII.GetBytes("Port: " + cloudPort);
             var ipEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 10000);
+            udpClient.Send(bytesToSend, bytesToSend.Length, ipEndpoint);
         } 
+      /*  /**
+         * Metoda generuje IDEndPoint na postawie podanego portu
+         #1#
         private IPEndPoint generateIPEndPoint(int port) {
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
             return ipEndPoint;
-        }
+        }*/
 
+        /**
+         * Metoda generuje XML na postawie klasy
+         */
         public string toXML() {
-            var stringwriter = new System.IO.StringWriter();
+            var stringwriter = new StringWriter();
             var serializer = new XmlSerializer(this.GetType());
             serializer.Serialize(stringwriter, this);
             return stringwriter.ToString();
         }
 
         public static Node LoadFromXMLString(string xmlText) {
-            var stringReader = new System.IO.StringReader(xmlText);
+            var stringReader = new StringReader(xmlText);
             var serializer = new XmlSerializer(typeof(Node));
             return serializer.Deserialize(stringReader) as Node;
         }
--- NetworkUtilitiesTests/NodeTest.cs
+++ NetworkUtilitiesTests/NodeTest.cs
@@ -4,6 +4,9 @@ using NetworkUtilities;
 using System.Diagnostics;
 using System.IO;
 using System.Xml.Serialization;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 namespace NetworkUtilitiesTests
 {
@@ -13,9 +16,53 @@ namespace NetworkUtilitiesTests
         [TestMethod]
         public void testNodeConstructor()
         {
-            
             Node node = new Node(5431, 3451);
             Console.Write(node.toXML());
-        } 
+        }
+
+        [TestMethod]
+        public void testNodeRecieve()
+        {
+            try {
+            Node node = new Node(5431, 3451);
+            string server = "127.0.0.1";
+            string message = "You can do it";
+            Int32 port = 3451;
+            TcpClient client = new TcpClient(server, port);
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer. 
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent: {0}", message);
+            // Receive the TcpServer.response.
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+            Debug.WriteLine("Received: {0}", responseData);
+            // Close everything.
+            stream.Close();
+            client.Close();
+               } 
+              catch (ArgumentNullException e) 
+              {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+                Debug.WriteLine("ArgumentNullException: {0}", e);
+              } 
+              catch (SocketException e) 
+              {
+                Console.WriteLine("SocketException: {0}", e);
+                Debug.WriteLine("SocketException: {0}", e);
+              }
+
+            Console.WriteLine("\n Press Enter to continue...");
+              Debug.WriteLine("\n Press Enter to continue...");
+              Console.Read();
+         }
     }
 }
