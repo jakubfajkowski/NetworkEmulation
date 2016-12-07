1fe1e481cc20570c2e16feff626986a6d3a2233f
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2016-12-07 23:37:16 +0100

    [NetworkEmulation] Changed upper bar. Implemented adding nodes.

---
+++ NetworkEmulation/NetworkNodePictureBox.cs
@@ -0,0 +1,9 @@
+﻿using NetworkEmulation.Properties;
+
+namespace NetworkEmulation {
+    class NetworkNodePictureBox : NodePictureBox {
+        public NetworkNodePictureBox() {
+            Image = Resources.NetworkNodeNotSelected;
+        }
+    }
+}
