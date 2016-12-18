6f74433499466c4a678823e88540ab5de157d552
Jarosław Steckiewicz <xitronix@mail.ru>
2016-12-18 14:37:07 +0100

    Connection SP - WinForm without methods

---
+++ NetworkEmulation/editor/element/ConnectionSP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetworkEmulation.network.element;
+
+namespace NetworkEmulation.editor.element {
+    public partial class ConnectionSP : Form {
+        public ConnectionSP() {
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e) {
+            var link= new LinkSerializableParameters() {
+                
+            };
+
+            new ConnectionSerializableParameters() {
+                
+            }
+            this.Close();
+        }
+    }
+}
