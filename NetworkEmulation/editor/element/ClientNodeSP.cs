0b4af29946e6faef67f88e02f30bc20c08688e77
Michal Kolodziej <M.Kolodziej@stud.elka.edu.pl>
2016-12-18 12:09:42 +0100

    Added ClientNodeSP

---
+++ NetworkEmulation/editor/element/ClientNodeSP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NetworkUtilities;
+using NetworkUtilities.element;
+
+namespace NetworkEmulation.editor.element {
+    public partial class ClientNodeSP : Form {
+        public ClientNodeSP() {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e) {
+            ClientNodeSerializableParameters param = new ClientNodeSerializableParameters {
+                Id = int.Parse(textBoxId.Text),
+                ClientName = textBoxClientName.Text,
+                ClientTable =
+                        new List<ClientTableRow>(new[]
+                            {new ClientTableRow(textBoxClientName.Text, int.Parse(textBoxClientPort.Text), int.Parse(textBoxVPI.Text), int.Parse(textBoxVCI.Text))}),
+                CloudPort = int.Parse(textBoxCloudPort.Text),
+                IpAddress = textBoxIpAddress.Text
+            };
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
