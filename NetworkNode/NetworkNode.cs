60ad940f1ec0c112736a088e0c5610bb910ee2d0
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2017-01-14 13:18:41 +0100

    Fixed NetworkNode bug.

--- NetworkNode/NetworkNode.cs
+++ NetworkNode/NetworkNode.cs
@@ -6,31 +6,76 @@ using NetworkUtilities.Element;
 
 namespace NetworkNode {
     public class NetworkNode : Node {
-        
-        // Czas po jakim komórki ATM zostaną spakowane w CCM
-        public static int MinLastAddTime { private get; set; } = 100;
-       
+        private Thread _networkNodeThread;
+
+        private bool _timeToQuit;
+
         public CommutationMatrix CommutationMatrix;
         public NetworkNodeAgent NetworkNodeAgent;
-        private LinkResourceManager linkResourceManager;
-
 
         public NetworkNode(NetworkNodeModel parameters)
-            : base(parameters.IpAddress, parameters.CableCloudListeningPort, parameters.CableCloudDataPort)
-        {
+            : base(parameters.IpAddress, parameters.CableCloudListeningPort, parameters.CableCloudDataPort) {
             CableCloudMessage.MaxAtmCellsNumber = parameters.MaxAtmCellsNumberInCableCloudMessage;
             NetworkNodeAgent.NmsPort = parameters.NetworkManagmentSystemListeningPort;
             NetworkNodeAgent = new NetworkNodeAgent(parameters.NetworkManagmentSystemDataPort, this);
-
             Console.WriteLine("Network Node \nNMS port: " + parameters.NetworkManagmentSystemDataPort +
                               "\nCableCloud port: " + parameters.CableCloudDataPort);
-
             CommutationMatrix = new CommutationMatrix(NetworkNodeAgent.GetCommutationTable(), parameters.NumberOfPorts);
             NetworkNodeAgent.SetCommutationMatrix(CommutationMatrix);
 
-            linkResourceManager = new LinkResourceManager(NetworkNodeAgent.GetCommutationTable(), parameters.NumberOfPorts, 300); // ZMIENIC CAPACITY
+            startThread();
         }
 
+        // Czas po jakim komórki ATM zostaną spakowane w CCM
+        public static int MinLastAddTime { private get; set; } = 100;
+
+        public void startThread() {
+            //_timeToQuit = false;
+            _networkNodeThread = new Thread(RunThread);
+            _networkNodeThread.Start();
+            CommutationMatrix.startThread();
+            NetworkNodeAgent.startThread();
+        }
+
+        public void shutdown() {
+            //_timeToQuit = true;
+            CommutationMatrix.shutdown();
+            NetworkNodeAgent.shutdown();
+        }
+
+        /* Wątek pobierający komórki ATM z portów wyjściowych pola komutacyjnego i wysyłający je do chmury kablowej */
+
+        private void RunThread() {
+            while (!_timeToQuit) {
+                var sent = false;
+                foreach (var port in CommutationMatrix.OutputPorts)
+                    if (((port.GetAtmCellNumber() != 0) &&
+                         ((DateTime.Now - port.GetLastAddTime()).TotalMilliseconds > MinLastAddTime)) ||
+                        (port.GetAtmCellNumber() > CableCloudMessage.MaxAtmCellsNumber)) {
+                        // Console.WriteLine("Różnica czasu: " + (DateTime.Now - port.GetLastAddTime()).TotalMilliseconds + "   if max: " + (port.GetAtmCellNumber() > CableCloudMessage.MaxAtmCellsNumber));
+                        var atmCells = new List<AtmCell>();
+
+                        var atmCellNumberInPort = port.GetAtmCellNumber();
+                        int atmCellNumberInMessage;
+                        if (atmCellNumberInPort > CableCloudMessage.MaxAtmCellsNumber)
+                            atmCellNumberInMessage = CableCloudMessage.MaxAtmCellsNumber;
+                        else
+                            atmCellNumberInMessage = atmCellNumberInPort;
+
+                        for (var i = 0; i < atmCellNumberInMessage; i++)
+                            atmCells.Add(port.GetAtmCell());
+
+                        //Console.WriteLine(DateTime.Now.Millisecond + "  Wysyłanie CableCloudMessage na port " +
+                        //                 message.PortNumber + " Liczba ATMCell: " + message.AtmCells.Count
+                        //                + " Port: " + port.GetPortNumber());
+                        var message = new CableCloudMessage(port.GetPortNumber(), atmCells);
+                        SendCableCloudMessage(message);
+                        sent = true;
+                    }
+                if (!sent)
+                    Thread.Sleep(10);
+            }
+        }
 
         protected override void Recieve(CableCloudMessage cableCloudMessage) {
             ReceiveCableCloudMessage(cableCloudMessage);        
---
+++ NetworkUtilities/ConnectionManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using NetworkUtilities.Log;
+using NetworkUtilities.Serialization;
+
+namespace NetworkUtilities {
+    public class ConnectionManager : LogObject {
+        protected readonly Dictionary<int, TcpClient> NodesTcpClients;
+        private UdpClient _connectionUdpClient;
+
+        protected ConnectionManager() {
+            NodesTcpClients = new Dictionary<int, TcpClient>();
+
+            Start();
+        }
+
+        public bool Online { get; private set; }
+
+        private void Start() {
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 10000);
+            _connectionUdpClient = new UdpClient(ipEndPoint);
+
+            ListenForConnectionRequests();
+        }
+
+        private void ListenForConnectionRequests() {
+            Task.Run(async () => {
+                using (_connectionUdpClient) {
+                    Online = true;
+                    while (true) {
+                        var receivedData = await _connectionUdpClient.ReceiveAsync();
+                        EstabilishNodeConnection((int) BinarySerializer.Deserialize(receivedData.Buffer));
+                    }
+                }
+            });
+        }
+
+        private void EstabilishNodeConnection(int port) {
+            var nodeTcpClient = new TcpClient();
+            try {
+                nodeTcpClient.Connect(IPAddress.Loopback, port);
+                NodesTcpClients.Add(port, nodeTcpClient);
+                UpdateState("Connected to Node on TCP port: " + port);
+                Listen(nodeTcpClient, port).Start();
+            }
+            catch (SocketException e) {
+                UpdateState(e.Message);
+            }
+        }
+
+        protected virtual Task Listen(TcpClient nodeTcpClient, int port) {
+            return null;
+        }
+
+        protected void SendBytes(byte[] data, TcpClient tcpClient) {
+            tcpClient.GetStream().Write(data, 0, data.Length);
+        }
+
+        public void Dispose() {
+            UpdateState("Shutting down.");
+            _connectionUdpClient.Close();
+        }
+    }
+}
