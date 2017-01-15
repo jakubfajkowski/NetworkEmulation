62188707abab68fac0deeb6b1d1076f0f7cece30
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2017-01-15 13:21:06 +0100

    Implemented useful methods in NetworkAddress.

--- NetworkUtilities/NetworkAddress.cs
+++ NetworkUtilities/NetworkAddress.cs
@@ -1,30 +1,69 @@
-﻿namespace NetworkUtilities {
+﻿using System;
+using System.Linq;
+using System.Windows.Forms.VisualStyles;
+
+namespace NetworkUtilities {
     public class NetworkAddress {
+        private const char Separator = '.';
         private readonly string _value;
+        public int Levels { get; private set; }
 
         public NetworkAddress(string value) {
             _value = value;
+            Levels = _value.Split(Separator).Length;
         }
 
         public static NetworkAddress Append(NetworkAddress networkAddress, int number) {
-            var value = networkAddress._value + "." + number;
+            var value = networkAddress._value + Separator + number;
             return new NetworkAddress(value);
         }
 
         public int GetId(int level) {
-            var ids = _value.Split('.');
+            var ids = _value.Split(Separator);
 
-            return int.Parse(ids[level - 1]);
+            return int.Parse(ids[level]);
         }
 
         public int GetLastId() {
-            var ids = _value.Split('.');
+            var ids = _value.Split(Separator);
 
             return int.Parse(ids[ids.Length - 1]);
         }
 
+        public int GetParentsId() {
+            var ids = _value.Split(Separator);
+
+            return int.Parse(ids[ids.Length - 2]);
+        }
+
+        public NetworkAddress GetParentsAddress() {
+            var ids = _value.Split(Separator);
+            var value = string.Join(Separator.ToString(), ids.Take(ids.Length - 2));
+            return new NetworkAddress(value);
+        }
+
+        public NetworkAddress GetRootFromBeggining(int level) {
+            var ids = _value.Split(Separator);
+            var value = string.Join(Separator.ToString(), ids.Take(level));
+            return new NetworkAddress(value);
+        }
+
+        public NetworkAddress GetRootFromEnd(int level) {
+            var ids = _value.Split(Separator);
+            var value = string.Join(Separator.ToString(), ids.Take(ids.Length - level));
+            return new NetworkAddress(value);
+        }
+
         public override string ToString() {
             return _value;
+        }
+
+        public override bool Equals(object obj) {
+            return _value.Equals(obj);
+        }
+
+        public override int GetHashCode() {
+            return _value.GetHashCode();
         }
     }
 }
